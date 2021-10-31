96e85ebc3d1e2840ab4b9de34624799728fbb907
andvsilva <andsilvadrcc@gmail.com>
2021-10-31 20:16:27 -0300

    database, Posts and Query

--- django-tutorial-Corey-Shafer/django_project/blog/views.py
+++ django-tutorial-Corey-Shafer/django_project/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'andvsilva',
-        'title': 'Blog Post 1',
-        'content': 'First post conent',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'First post conent',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all() # Query.
     }
     return render(request, 'blog/home.html', context)
 
